2422b934ec6ca7bb2077c03f796ce11c9e9ec3f3
Quimorax <nikitademch24@gmail.com>
2021-09-22 14:42:14 +0300

    Rewrite unittests in "test_check_input.py" and in "test_make_move.py".

--- tests/test_check_input.py
+++ tests/test_check_input.py
@@ -1,29 +1,32 @@
-import unittest
 import main
 from utilities.matrix_utilities import make_matrix
 from utilities.check_utilities import check_input
 
+import pytest
 
-class TestCheckInput(unittest.TestCase):
-    matrix = make_matrix(main.desk_size)
+matrix = make_matrix(main.desk_size)
 
-    def test_input_too_large_coordinate(self):
-        with self.assertRaises(ValueError) as error:
-            check_input(main.desk_size ** 2 + 1, TestCheckInput.matrix)
-        self.assertEqual('Too large coordinate', error.exception.args[0])
 
-    def test_input_too_small_coordinate(self):
-        with self.assertRaises(ValueError) as error:
-            check_input(0, TestCheckInput.matrix)
-        self.assertEqual('Too small coordinate', error.exception.args[0])
+def test_input_too_large_coordinate():
+    with pytest.raises(ValueError) as error:
+        check_input(main.desk_size ** 2 + 1, matrix)
+    assert 'Too large coordinate' == error.value.args[0]
 
-    def test_not_available_coordinate(self):
-        matrix = TestCheckInput.matrix.copy()
-        matrix[0][0] = main.players['cross']
-        with self.assertRaises(ValueError) as error:
-            check_input(1, matrix)
-        self.assertEqual('Coordinate is already occupied', error.exception.args[0])
+
+def test_input_too_small_coordinate():
+    with pytest.raises(ValueError) as error:
+        check_input(0, matrix)
+    assert 'Too small coordinate' == error.value.args[0]
+
+
+def test_not_available_coordinate():
+    global matrix
+    matrix = matrix.copy()
+    matrix[0][0] = main.players['cross']
+    with pytest.raises(ValueError) as error:
+        check_input(1, matrix)
+    assert 'Coordinate is already occupied' == error.value.args[0]
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
--- tests/test_make_move.py
+++ tests/test_make_move.py
@@ -1,38 +1,39 @@
-import unittest
 import main
 from utilities.matrix_utilities import make_matrix
 
+import pytest
 
-class TestMakeMove(unittest.TestCase):
-    """All tests test two player names"""
 
-    def test_last_coordinate(self):
-        last_step_coordinate = main.desk_size ** 2
-        for player in main.players:
-            matrix = make_matrix(main.desk_size)
-            print(player)
-            print(f'Enter this: {last_step_coordinate}')
-            main.make_move(matrix, player)
-            self.assertEqual(all(last_step_coordinate not in list_ for list_ in matrix), True)
+def test_last_coordinate():
+    last_step_coordinate = main.desk_size ** 2
+    for player in main.players:
+        matrix = make_matrix(main.desk_size)
+        main.input = lambda message: last_step_coordinate
+        main.make_move(matrix, player)
+        assert all(last_step_coordinate not in list_ for list_ in matrix) is True
 
-    def test_third_coordinate(self):
-        third_step_coordinate = main.desk_size  # because matrix size desk_size ** 2
-        for player in main.players:
-            matrix = make_matrix(main.desk_size)
-            print(player)
-            print(f'Enter this: {third_step_coordinate}')
-            main.make_move(matrix, player)
-            self.assertEqual(all(third_step_coordinate not in list_ for list_ in matrix), True)
 
-    def test_first_coordinate(self):
-        first_step_coordinate = 1
-        for player, player_designation in main.players.items():
-            matrix = make_matrix(main.desk_size)
-            print(player)
-            print(f'Enter this: {first_step_coordinate}')
-            main.make_move(matrix, player)
-            self.assertEqual(all(first_step_coordinate not in list_ for list_ in matrix), True)
+def test_third_coordinate():
+    third_step_coordinate = main.desk_size  # because matrix size desk_size ** 2
+    for player in main.players:
+        matrix = make_matrix(main.desk_size)
+        main.input = lambda message: third_step_coordinate
+        main.make_move(matrix, player)
+        assert all(third_step_coordinate not in list_ for list_ in matrix) is True
+
+
+def test_first_coordinate():
+    first_step_coordinate = 1
+    for player, player_designation in main.players.items():
+        matrix = make_matrix(main.desk_size)
+        main.input = lambda message: first_step_coordinate
+        main.make_move(matrix, player)
+        assert all(first_step_coordinate not in list_ for list_ in matrix) is True
+
+
+def teardown():
+    main.input = input
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
